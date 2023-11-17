0c9358da49ee9a2e406ad12cec767494ce7cedb7
jennifer-rooney002 <144469400+jennifer-rooney002@users.noreply.github.com>
2023-11-17 17:17:50 -0500

    uploaded models and a new ds you need
    updated models with a new ds called "hemisphere" that has if the site (country) is in the northern or southern hemisphere. jarvis suggested interacting this with month is a good way to account for seasonality

--- Final project/models_mlm_proj.R
+++ Final project/models_mlm_proj.R
@@ -1,4 +1,7 @@
 pm <- read.csv("C:\\Users\\jenni\\Downloads\\final_ds.csv", header=TRUE, sep=",", na.strings=c("","NA"))
+hemis <- read.csv("C:\\Users\\jenni\\Downloads\\hemisphere.csv", header=TRUE, sep=",", na.strings=c("","NA"))
+
+pm <- merge(pm, hemis, by.x ="country", by.y ="Country.Name")
 
 library(dplyr)
 library(tidyr)
@@ -21,11 +24,11 @@ pm$month <- as.factor(pm$month)
 ## model 1 is residential percent change
 library(lmerTest)
 detach("package:lmerTest", unload = TRUE)
-install.packages("lme4")
+#install.packages("lme4")
 library(lme4)
 pm$hdicode <- as.factor(pm$hdicode)
-pm$hdicode <- relevel(pm$hdicode, ref = "Very High")
 pm$hdicode <- factor(pm$hdicode, levels = c("Low", "Medium", "High", "Very High"))
+pm$hdicode <- relevel(pm$hdicode, ref = "Very High")
 pm <- pm %>%
   mutate(week_index = ceiling(time_index / 7))
 pm <- pm %>%
@@ -33,8 +36,8 @@ pm <- pm %>%
 pm <- pm %>%
   mutate(workplaces_reversed = -workplaces_percent_change_from_baseline)
 
-x <- lmer(a_mean ~ workplaces_reversed*hdicode  +
-              (1 + month + time_index | Mobility_SiteName), control = lmerControl(optimizer = "bobyqa"),
+x <- lmer(a_mean ~ workplaces_reversed*hdicode  + time_index + month*hemisphere +
+              (1 + time_index | Mobility_SiteName), control = lmerControl(optimizer = "bobyqa"),
      data = pm)
 
 summary(x)
@@ -46,9 +49,13 @@ library(ggplot2)
 interaction_effects <- ggeffect(x, c("workplaces_reversed", "hdicode"))
 plot(interaction_effects)
 
+### plot month*hemisphere just to see how its handling seasonality
+interaction_effects2 <- ggeffect(x, c("month", "hemisphere"))
+plot(interaction_effects2)
+
 ## plot random effects
 library(sjPlot)
 custom_palette <- rainbow(length(unique(pm$Mobility_SiteName)))
 plot_model(x, type="pred",
-           terms=c("month","Mobility_SiteName"),
+           terms=c("time_index", "Mobility_SiteName"),
            pred.type="re", ci.lvl = NA, colors = custom_palette)
